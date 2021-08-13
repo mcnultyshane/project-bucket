dd1a61e2765a317ab38bc8ccca79b265652e99e3
KIMBERLYSEDDON <82526676+KIMBERLYSEDDON@users.noreply.github.com>
2021-08-13 10:34:51 -0400

    signup logic/ nav clean up

--- client/src/App.js
+++ client/src/App.js
@@ -65,7 +65,7 @@ function App() {
             <Route exact path="/profile" component={Profile}/>
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
           </Switch>
-
+          {/* <Footer /> */}
         </div>
         </ThemeProvider>
       </Router>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,87 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { alpha, makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import Badge from "@material-ui/core/Badge";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
+import { AppBar, Toolbar, IconButton, Typography, InputBase, Badge, MenuItem, Menu, Button, ButtonGroup, Modal } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import ListAltTwoToneIcon from "@material-ui/icons/ListAltTwoTone";
 import HomeTwoTone from "@material-ui/icons/HomeTwoTone";
 import MoreIcon from "@material-ui/icons/MoreVert";
-import { Button, ButtonGroup, Modal } from "@material-ui/core";
-import Signup from '../Signup'
+import Signup from "../Signup";
+import useStyles from "./styles";
+import Auth from '../../utils/auth'
 
-const useStyles = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
-  },
-  //   menuButton: {
-  //     marginRight: theme.spacing(2),
-  //   },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block",
-    },
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto",
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex",
-    },
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-  },
-}));
-
-export default function Navbar({ theme }) {
+export default function Navbar() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
@@ -128,10 +57,29 @@ export default function Navbar({ theme }) {
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
+      {Auth.loggedIn() ? (
+        <>
+        <MenuItem> Log In </MenuItem>
+        <MenuItem onClick={handleOpen}>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          <Signup />
+        </Modal>
+        Sign Up
+        </MenuItem>
+        </>
+      ): (
+        <>
       <MenuItem onClick={handleMenuClose} component={Link} to="/profile">
         My Profile
       </MenuItem>
       <MenuItem onClick={handleMenuClose}>Log Out</MenuItem>
+      </>
+      )}
     </Menu>
   );
   // MOBILE VIEW MENU vvvvvvvvv
@@ -216,22 +164,25 @@ export default function Navbar({ theme }) {
                 <ListAltTwoToneIcon />
               </Badge>
             </IconButton>
+            {Auth.loggedIn()? (
             <IconButton onClick={handleProfileMenuOpen}>
               <AccountCircle />
-            </IconButton>
+            </IconButton> ) : (
             <ButtonGroup variant="contained" color="secondary">
-            <Button>Log In</Button>
+              <Button>Log In</Button>
               <Button onClick={handleOpen}>
                 <Modal
                   open={open}
                   onClose={handleClose}
                   aria-labelledby="simple-modal-title"
-                  aria-describedby="simple-modal-description">
+                  aria-describedby="simple-modal-description"
+                >
                   <Signup />
                 </Modal>
-                 Sign Up
+                Sign Up
               </Button>
             </ButtonGroup>
+            )}
           </div>
           <div className={classes.sectionMobile}>
             <IconButton
@@ -251,126 +202,3 @@ export default function Navbar({ theme }) {
     </div>
   );
 }
-
-// import React, { useState } from "react";
-// import { alpha, makeStyles } from '@material-ui/core/styles';
-// import { Link } from "react-router-dom";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from '@material-ui/core/Toolbar';
-// import IconButton from '@material-ui/core/IconButton';
-// import Typography from '@material-ui/core/Typography';
-// import InputBase from '@material-ui/core/InputBase';
-// import Badge from '@material-ui/core/Badge';
-// import SearchIcon from '@material-ui/icons/Search';
-// import AccountCircle from '@material-ui/icons/AccountCircle';
-// import ListAltTwoToneIcon from '@material-ui/icons/ListAltTwoTone'
-// import HomeTwoTone from '@material-ui/icons/HomeTwoTone'
-
-// const useStyles = makeStyles((theme) => ({
-//     search: {
-//         position: 'relative',
-//         borderRadius: theme.shape.borderRadius,
-//         backgroundColor: alpha(theme.palette.common.white, 0.15),
-//         '&:hover': {
-//           backgroundColor: alpha(theme.palette.common.white, 0.25),
-//         },
-//         marginRight: theme.spacing(2),
-//         marginLeft: 0,
-//         width: '100%',
-//         [theme.breakpoints.up('sm')]: {
-//           marginLeft: theme.spacing(3),
-//           width: 'auto',
-//         },
-//     },
-//     searchIcon: {
-//         padding: theme.spacing(0, 2),
-//         height: '100%',
-//         position: 'absolute',
-//         pointerEvents: 'none',
-//         display: 'flex',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//       },
-//     inputRoot: {
-//         color: 'inherit',
-//       },
-//       inputInput: {
-//         padding: theme.spacing(1, 1, 1, 0),
-//         // vertical padding + font size from searchIcon
-//         paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-//         transition: theme.transitions.create('width'),
-//         width: '100%',
-//         [theme.breakpoints.up('md')]: {
-//           width: '20ch',
-//         },
-//       },
-//       sectionDesktop: {
-//         display: 'none',
-//         [theme.breakpoints.up('md')]: {
-//           display: 'flex',
-//         },
-//       },
-//       sectionMobile: {
-//         display: 'flex',
-//         [theme.breakpoints.up('md')]: {
-//           display: 'none',
-//         },
-//       },
-// } ))
-
-// const Navbar = () => {
-//     const classes = useStyles();
-
-//   return (
-//     <>
-//       <AppBar position="static">
-//         <Toolbar>
-//         <Typography variant="h6" >
-//             B*ucket
-//           </Typography>
-//           <div className={classes.search}>
-//             <div className={classes.searchIcon}>
-//               <SearchIcon />
-//             </div>
-//             <InputBase
-//               placeholder="Search…"
-//               classes={{
-//                 root: classes.inputRoot,
-//                 input: classes.inputInput,
-//               }}
-//               inputProps={{ 'aria-label': 'search' }}
-//             />
-//           </div>
-//           <div className={classes.grow} />
-//           <div className={classes.sectionDesktop}>
-//             <IconButton aria-label="show 4 new mails" color="inherit">
-//               <Badge color="secondary">
-//                 <ListAltTwoToneIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton aria-label="show 17 new notifications" color="inherit">
-//               <Badge badgeContent={17} color="secondary">
-//                 <HomeTwoTone />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               edge="end"
-//               aria-label="account of current user"
-//             //   aria-controls={menuId}
-//               aria-haspopup="true"
-//             //   onClick={}
-//               color="inherit"
-//             >
-//               <AccountCircle />
-//             </IconButton>
-//           </div>
-
-//           {/* <Button color="inherit">Login</Button> */}
-
-//         </Toolbar>
-//       </AppBar>
-//     </>
-//   );
-// };
-
-// export default Navbar;
