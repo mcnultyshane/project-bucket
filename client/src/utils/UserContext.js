4ef1066c93bcac4cadb88879c13fe5c714d1322a
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-08-14 18:13:05 -0400

    pulled in edits to campaign queries. merged, my new additions in client profiles

--- client/src/utils/UserContext.js
+++ client/src/utils/UserContext.js
@@ -1,43 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { createContext } from "react";
 import { useQuery } from "@apollo/client";
-import { GET_ME } from "../utils/queries";
+import { QUERY_ME } from "../utils/queries";
 
 export const UserContext = createContext();
 
 const UserProvider = (props) => {
   
-  const { loading, data } = useQuery(GET_ME);
-//     // ,
-//     //   {
-//     //   variables: { userId: userId },
-//     // }
-//     );
+  const { loading, data } = useQuery(QUERY_ME);
+
 
   const userData = data?.me || [];
-// //   const [currentUser, setCurrentUser] = useState({
-// //     email: "",
-// //   });
+
   console.log(userData);
 
-// useEffect(() => {
-//     console.log(this);
-//     setCurrentUser({
-// //         email: userData.email.value,
 
-//     })
-//   },[1])
-// if (userData = {}){
-//     setCurrentUser(...userData);
-// }
-
-  // const getCurrentUser = async function (userData) {
-
-  //     setCurrentUser(userData);
-
-  //   };
-
-  //   getCurrentUser(userData)
   return (
     <UserContext.Provider  value={{ currentUser: "currentUser" }} {...props} />
   );
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,7 +16,7 @@ const resolvers = {
       me: async (parent, args, context) => {
 
         if (context.user) {
-          return User.findOne({ _id: context.user._id }).populate("bucketLists");;
+          return User.findOne({ _id: context.user._id });
         }
         throw new AuthenticationError("You need to be logged in!");
       },
