bac6450521a4d1df338f77c4a6e1b9309774cc8b
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-08-14 12:29:45 -0400

    added front end for a profile page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -27,3 +27,40 @@ export const QUERY_USERS = gql`
     }
   }
 `;
+export const QUERY_SINGLE_USER = gql`
+  query singleProfile($profileId: ID!) {
+    profile(profileId: $profileId) {
+      _id
+      email
+      username
+      firstName
+      lastName
+      password
+      bucketList {
+        campaign {
+          title
+          description
+          updates{
+            content
+            dateCreated
+          }
+          dateCreated
+        }
+      }
+      location {
+        address
+      }
+      avatar
+    }
+  }
+  `;
+  export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      email
+      username
+    }
+  }
+`;
+
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,9 +8,10 @@ const resolvers = {
       users: async () => {
         return User.find();
       },
-      getSingleUser: async (parent, { username }) => {
-        return User.findOne({ username });
+      getSingleUser: async (parent, { userId }) => {
+        return User.findOne({ _id: userId });
       },
+      
   
       me: async (parent, args, context) => {
         if (context.user) {
@@ -78,6 +79,7 @@ const resolvers = {
       }
         //throw new AuthenticationError('Not logged in');  
     },
+
     
     updateCampaign: async (parent, {campaignId, content}) => {
       return Campaign.findByIdAndUpdate(
